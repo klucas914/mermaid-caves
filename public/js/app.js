a51e90c5f9acbbeef018ea400383cbe5ea4e8278
Kristin Lucas <kristinardell@yahoo.com>
2015-07-05 14:36:40 -0400

    allow user to travel diagonally

--- public/js/app.js
+++ public/js/app.js
@@ -72,6 +72,7 @@ window.onload = function() {
     game.physics.arcade.collide(player, layer);
 
     player.body.velocity.x = 0;
+    player.body.velocity.y = 0;
 
     if (cursors.left.isDown)
     {
@@ -83,7 +84,7 @@ window.onload = function() {
             facing = 'left';
         }
     }
-    else if (cursors.right.isDown)
+    if (cursors.right.isDown)
     {
         player.body.velocity.x = 150;
 
@@ -93,17 +94,18 @@ window.onload = function() {
             facing = 'right';
         }
     }
-    else if (cursors.down.isDown)
+    if (cursors.down.isDown)
     {
         player.body.velocity.y = 150;
         player.animations.stop();
     }
-    else if (cursors.up.isDown)
+    if (cursors.up.isDown)
     {
         player.body.velocity.y = -150;
         player.animations.stop();
     }
-    else
+
+    if (!cursors.left.isDown && !cursors.right.isDown && !cursors.up.isDown && !cursors.down.isDown)
     {
         if (facing != 'idle')
         {
